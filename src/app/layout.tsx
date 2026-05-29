1046064a165b0b4b2eca9ae98002f53db2ccd602
Valentin <zaheil444@gmail.com>
2026-05-28 21:08:37 -0300

    logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -91,11 +91,7 @@ export const metadata: Metadata = {
     title: "DolarARG",
   },
   icons: {
-    icon: [
-      { url: "/icons/icon.svg", type: "image/svg+xml" },
-      { url: "/icons/icon-192.png", sizes: "192x192", type: "image/png" },
-      { url: "/icons/icon-512.png", sizes: "512x512", type: "image/png" },
-    ],
+    icon: "/favicon.ico",
     apple: [
       { url: "/icons/icon-192.png", sizes: "192x192" },
     ],
